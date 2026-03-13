85f5dc81cc7ae8c6f60834f0e8553d083925f6f1
pixelpawnshop <christopher.guth1@gmail.com>
2026-03-13 20:40:10 +0100

    changed base path for gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/osm-feature-extractor/',
+  base: '/openstreetmap-feature-extractor/',
   server: {
     port: 3000,
   },
